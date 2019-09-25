82e635e20a6cbcb88f2bba8e3d8d31267ed494dc
lz <2120140200@mail.nankai.edu.cn>
2019-09-25 23:18:32 +0800

    update behappy

--- be_happy.py
+++ be_happy.py
@@ -2,9 +2,10 @@
 import cv2
 import time
 import imageio
+import numpy as np
+
 
 from lib.core.api.facer import FaceAna
-from lib.core.LK.lk import GroupTrack
 
 from lib.core.headpose.pose import get_head_pose, line_pairs
 facer = FaceAna()
@@ -19,14 +20,15 @@ def video(video_path_or_cam):
     while 1:
         counter+=1
 
-        ret, img = vide_capture.read()
 
-        img = cv2.resize(img, None, fx=1.4, fy=1.4)
-        #img = cv2.resize(img, (1280 , 960))
-        #img=img[220:1000,:,:]
-        img_show = img.copy()
+        ret, image = vide_capture.read()
+        #image = image[100:1000, :, :]
+        image=cv2.resize(image, None, fx=2., fy=2.)
+        pattern = np.zeros_like(image)
 
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_show = image.copy()
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
         star=time.time()
         boxes, landmarks, states = facer.run(img)
 
@@ -39,8 +41,13 @@ def video(video_path_or_cam):
             #######head pose
             reprojectdst, euler_angle=get_head_pose(landmarks[face_index],img_show)
 
-            # for start, end in line_pairs:
-            #     cv2.line(img_show, reprojectdst[start], reprojectdst[end], (0, 0, 255),2)
+            if args.mask:
+                face_bbox_keypoints = np.concatenate(
+                    (landmarks[face_index][:17, :], np.flip(landmarks[face_index][17:27, :], axis=0)), axis=0)
+
+                pattern = cv2.fillPoly(pattern, [face_bbox_keypoints.astype(np.int)], (1., 1., 1.))
+            for start, end in line_pairs:
+                cv2.line(img_show, reprojectdst[start], reprojectdst[end], (0, 0, 255),2)
 
             cv2.putText(img_show, "X: " + "{:7.2f}".format(euler_angle[0, 0]), (20, 20), cv2.FONT_HERSHEY_SIMPLEX,
                         0.75, (0, 0, 0), thickness=2)
@@ -52,8 +59,15 @@ def video(video_path_or_cam):
             for landmarks_index in range(landmarks[face_index].shape[0]):
 
                 x_y = landmarks[face_index][landmarks_index]
-                cv2.circle(img_show, (int(x_y[0]), int(x_y[1])), 4,
+                cv2.circle(img_show, (int(x_y[0]), int(x_y[1])), 3,
                            (222, 222, 222), -1)
+
+
+
+
+
+
+
 
         img_show = cv2.cvtColor(img_show, cv2.COLOR_BGR2RGB)
         img_show=cv2.resize(img_show,None,fx=0.4,fy=0.4)
@@ -61,12 +75,10 @@ def video(video_path_or_cam):
 
         if start_holder:
             buff.append(img_show)
-        #counter+=1
-        # fps=1./duration
-        # # cv2.putText(img_show, 'fps %f'%fps, (10, 100),
-        # #             cv2.FONT_HERSHEY_SIMPLEX, 1,
-        # #             (255, 222, 255), 2)
-        # videoWriter.write(img_show)
+        if args.mask:
+            cv2.namedWindow("masked", 0)
+            cv2.imshow("masked", img * pattern)
+
         cv2.namedWindow("capture", 0)
         cv2.imshow("capture", img_show)
         key=cv2.waitKey(1)
@@ -83,9 +95,11 @@ if __name__=='__main__':
 
     parser = argparse.ArgumentParser(description='Start train.')
     parser.add_argument('--video', dest='video', type=str, default=None, \
-                        help='the num of the classes (default: 100)')
+                        help='the camera id (default: 0)')
     parser.add_argument('--cam_id', dest='cam_id', type=int, default=0, \
-                        help='the camre to use')
+                        help='the camera to use')
+    parser.add_argument('--mask', dest='mask', type=bool, default=False, \
+                        help='mask the face or not')
     args = parser.parse_args()
 
 
