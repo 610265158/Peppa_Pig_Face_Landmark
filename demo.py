d7aa0f06de13d9285a8d7fcb12a019763b19c758
lz <2120140200@mail.nankai.edu.cn>
2023-05-05 15:32:48 +0800

    ff

--- demo.py
+++ demo.py
@@ -23,7 +23,7 @@ def video(video_path_or_cam):
 
             star=time.time()
             result = facer.run(image)
-            print(result)
+
             duration=time.time()-star
             #print('one iamge cost %f s'%(duration))
 
@@ -79,10 +79,8 @@ def images(image_dir):
 
         img_show = image.copy()
 
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
         star=time.time()
-        boxes, landmarks, states = facer.run(image)
+        result = facer.run(image)
 
         ###no track
         facer.reset()
@@ -90,41 +88,28 @@ def images(image_dir):
         duration=time.time()-star
         print('one iamge cost %f s'%(duration))
 
-        for face_index in range(landmarks.shape[0]):
+        for face_index in range(len(result)):
 
             #######head pose
             #reprojectdst, euler_angle=get_head_pose(landmarks[face_index],img_show)
 
-            if args.mask:
-                face_bbox_keypoints = np.concatenate(
-                    (landmarks[face_index][:17, :], np.flip(landmarks[face_index][17:27, :], axis=0)), axis=0)
+            cur_face_kps = result[face_index]['kps']
+            cur_face_kps_score = result[face_index]['scores']
+            for landmarks_index in range(cur_face_kps.shape[0]):
 
-                pattern = cv2.fillPoly(pattern, [face_bbox_keypoints.astype(np.int)], (1., 1., 1.))
-
-
-            # for start, end in line_pairs:
-            #     cv2.line(img_show, reprojectdst[start], reprojectdst[end], (0, 0, 255),2)
-            #
-            # cv2.putText(img_show, "X: " + "{:7.2f}".format(euler_angle[0, 0]), (20, 20), cv2.FONT_HERSHEY_SIMPLEX,
-            #             0.75, (0, 0, 0), thickness=2)
-            # cv2.putText(img_show, "Y: " + "{:7.2f}".format(euler_angle[1, 0]), (20, 50), cv2.FONT_HERSHEY_SIMPLEX,
-            #             0.75, (0, 0, 0), thickness=2)
-            # cv2.putText(img_show, "Z: " + "{:7.2f}".format(euler_angle[2, 0]), (20, 80), cv2.FONT_HERSHEY_SIMPLEX,
-            #             0.75, (0, 0, 0), thickness=2)
-
-            for landmarks_index in range(landmarks[face_index].shape[0]):
-
-                x_y = landmarks[face_index][landmarks_index]
-                cv2.circle(img_show, (int(x_y[0]), int(x_y[1])), 3,
-                           (222, 222, 222), -1)
-
+                x_y = cur_face_kps[landmarks_index]
+                score = cur_face_kps_score[landmarks_index]
+                # color = (255, 255, 255)
+                if score > 0.8:
+                    color = (255, 255, 255)
+                else:
+                    color = (0, 0, 255)
+                cv2.circle(img_show, (int(x_y[0]), int(x_y[1])),
+                           color=color, radius=1, thickness=2)
 
         cv2.namedWindow("capture", 0)
         cv2.imshow("capture", img_show)
 
-        if args.mask:
-            cv2.namedWindow("masked", 0)
-            cv2.imshow("masked", image*pattern)
 
         key=cv2.waitKey(0)
         if key==ord('q'):
